971f853d05ad41fc60b44438e4f23a709bc9cd0e
sejalpatel2003 <patelsejal71295@gmail.com>
2022-03-29 09:03:57 +0530

    ADDED:[SEJAL] Program for surface area and volume of cuboid

---
+++ CUBOID.C
@@ -0,0 +1,21 @@
+/*Program to calculate surface area and volume of cuboid*/
+/*surface area=2*(l*b+b*h+l*h)*/
+/*volume=l*b*h*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int l,b,h,surfacearea,volume;
+	clrscr();
+	printf("\nEnter the value of Length=");
+	scanf("%d",&l);
+	printf("\nEnter the value of Breadth=");
+	scanf("%d",&b);
+	printf("\nEnter the value of Height=");
+	scanf("%d",&h);
+	surfacearea=2*(l*b+b*h+l*h);
+	printf("\nSurface area of Cuboid=%d",surfacearea);
+	volume=l*b*h;
+	printf("\nVolume of Cuboid=%d",volume);
+	getch();
+}
